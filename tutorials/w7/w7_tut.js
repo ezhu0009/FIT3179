3eabf410cc986b7705cf0ca6848ae0f621ff93b3
ezhu0009 <ezhu0009@student.monash.edu>
2024-09-09 13:32:25 +1000

    Uploaded Week 7 tutorial files

---
+++ tutorials/w7/w7_tut.js
@@ -0,0 +1,12 @@
+
+
+
+var end = 2                       // indiv statement
+for (let i = 0; i < end; i++) {     // Start foa block
+    console.log(1 * 2)              // single statement inside block
+}                                   // end of the block
+
+
+var message; // variable declaration 
+message = "FIT3179 - Data Vis"; // assigned a string value
+// alert(message); //access a variable
